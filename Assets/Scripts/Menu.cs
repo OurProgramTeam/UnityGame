3001cb431fcc7510bd5473c21720825054630737
CathyTsar <tennleston@gmail.com>
2017-10-29 18:51:21 +0300

    Добавила меню: бекгрануд, кнопки начала игры и выхода из игры.

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts
+{
+    public class Menu : MonoBehaviour
+    {
+
+        public void StartGame()
+        {
+            Application.LoadLevel(1);
+        }
+        public void Exit()
+        {
+            Application.Quit();
+        }
+    }
+}
