b4de93af5546029b52e4819193acd830e3c09a79
SashaOcheev <OcheevSasha@gmail.com>
2017-11-05 20:50:56 +0300

    решены проблемы с точностью

--- Assets/Scripts/Gameplay/Flip/Transformations/TransfromComponents.cs
+++ Assets/Scripts/Gameplay/Flip/Transformations/TransfromComponents.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System;
 
 namespace Scripts.Gameplay.Flip.Transformations
 {
@@ -10,10 +11,26 @@ namespace Scripts.Gameplay.Flip.Transformations
         public TransfromComponents(Transform transfrom)
         {
             var srcPosition = transfrom.position;
-            Position = new Vector3(srcPosition.x, srcPosition.y, srcPosition.z);
+            Position = new Vector3(
+                Round(srcPosition.x),
+                Round(srcPosition.y),
+                Round(srcPosition.z)
+            );
 
             var srcRotation = transfrom.rotation;
-            Rotation = new Quaternion(srcRotation.x, srcRotation.y, srcRotation.z, srcRotation.w);
+            Rotation = new Quaternion(
+                Round(srcRotation.x),
+                Round(srcRotation.y),
+                Round(srcRotation.z),
+                Round(srcRotation.w)
+            );
+        }
+
+        private float Round(float number)
+        {
+            const int DigitsAfterDot = 2;
+
+            return (float)Math.Round(number, DigitsAfterDot);
         }
     }
 }
